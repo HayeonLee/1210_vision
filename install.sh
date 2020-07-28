bc09f26a4e8820e1592202291624dd78870a3b47
ModestyJ <modesty.j@gmail.com>
2020-07-28 20:07:41 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 conda deactivate
 conda env create -f env.yml
 conda activate 0731_vision
+pip install 'tensorflow-estimator<1.15.0rc0,>=1.14.0rc0' --force-reinstall
+pip install -U gast==0.2.2
 python train.py
+
